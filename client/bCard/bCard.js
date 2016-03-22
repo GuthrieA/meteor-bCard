0b969862ed3fa272946403fcc7e8c402039d8e3f
Andrew Guthrie <AndrewHopkinsGuthrie@Gmail.com>
2016-03-22 12:38:22 -0400

    Modify bCard.html and bCard.js

---
+++ client/bCard/bCard.js
@@ -0,0 +1,17 @@
+Template.bCard.helpers({
+  firstName: function(){
+    return Session.get('firstName');
+  },
+  lastName: function(){
+    return Session.get('lastName');
+  },
+  githubLink: function(){
+    return Session.get('githubLink');
+  },
+  gender: function(){
+      return Session.get('gender');
+  },
+  quote: function(){
+    return Session.get('quote');
+  }
+});
